e062cda2e18b8cd5a20e4024f0d50026fd7b7b16
Jelmer Visser <j.visser@webuildapps.com>
2016-04-23 15:03:48 +0200

    Updated walk code

--- new_walk.py
+++ new_walk.py
@@ -1,7 +1,8 @@
-__author__ = 'Jelmer Visser'
-
-from ax12 import Ax12
 import time
+from ax12 import Ax12
+ax12 = Ax12()
+
+__author__ = 'Jelmer Visser'
 
 speed = 1
 
@@ -9,49 +10,43 @@ firstServoMove = None
 secondServoMove = None
 thirdServoMove = None
 
+
 def resetMoves():
     firstServoMove = None
     secondServoMove = None
     thirdServoMove = None
 
+
 def waitForServos():
-    while firstServoFirstMove == None and secondServoFirstMove == None and thirdServoFirstMove == None:
+    while firstServoMove and secondServoMove and thirdServoMove is None:
         time.sleep(0.00005)
     resetMoves()
 
+
 def walk():
     while True:
-        firstServoFirstMove = ax12.moveSpeed(10, 512, int(512 * speed))
-        secondServoFirstMove = ax12.moveSpeed(11, 355, int(245 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 110, int(511 * speed))
-
+        firstServoMove = ax12.moveSpeed(10, 512, int(512 * speed))
+        secondServoMove = ax12.moveSpeed(11, 355, int(245 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 110, int(511 * speed))
         waitForServos()
-
-        firstServoFirstMove = ax12.moveSpeed(10, 665, int(512 * speed))
-        secondServoFirstMove = ax12.moveSpeed(11, 428, int(245 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 263, int(511 * speed))
-
+        firstServoMove = ax12.moveSpeed(10, 665, int(512 * speed))
+        secondServoMove = ax12.moveSpeed(11, 428, int(245 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 263, int(511 * speed))
         waitForServos()
-
-        secondServoFirstMove = ax12.moveSpeed(11, 467, int(512 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 291, int(371 * speed))
-
+        secondServoMove = ax12.moveSpeed(11, 467, int(512 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 291, int(371 * speed))
         waitForServos()
-
-        firstServoFirstMove = ax12.moveSpeed(10, 512, int(512 * speed))
-        secondServoFirstMove = ax12.moveSpeed(11, 404, int(209 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 138, int(509 * speed))
-
+        firstServoMove = ax12.moveSpeed(10, 512, int(512 * speed))
+        secondServoMove = ax12.moveSpeed(11, 404, int(209 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 138, int(509 * speed))
         waitForServos()
-
-        firstServoFirstMove = ax12.moveSpeed(10, 358, int(512 * speed))
-        secondServoFirstMove = ax12.moveSpeed(11, 467, int(209 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 291, int(509 * speed))
-
+        firstServoMove = ax12.moveSpeed(10, 358, int(512 * speed))
+        secondServoMove = ax12.moveSpeed(11, 467, int(209 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 291, int(509 * speed))
         waitForServos()
+        secondServoMove = ax12.moveSpeed(11, 428, int(512 * speed))
+        thirdServoMove = ax12.moveSpeed(12, 263, int(371 * speed))
 
-        secondServoFirstMove = ax12.moveSpeed(11, 428, int(512 * speed))
-        thirdServoFirstMove = ax12.moveSpeed(12, 263, int(371 * speed))
 
 def main():
     walk()
