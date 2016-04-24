dd7a63cb8f004502c2f5c799665031684b4b4e09
Jelmer Visser <j.visser@webuildapps.com>
2016-04-24 16:23:56 +0200

    Support for degrees

--- dynamixel_lab_lib_walk_test.py
+++ dynamixel_lab_lib_walk_test.py
@@ -1,30 +1,42 @@
 from dxl.dxlchain import DxlChain
 
+__author__ = 'Jelmer Visser'
+
 # Open the serial device
 chain = DxlChain("/dev/ttyAMA0", rate=1000000)
 
 speed = 1
 
-chain.goto(10, 512, speed=int(512 * speed))
-chain.goto(11, 355, speed=int(245 * speed))
-chain.goto(12, 110, speed=int(511 * speed))
+def degreesToBits(degrees):
+    return (1023 * degrees) / 300
 
-chain.goto(10, 665, speed=int(512 * speed))
-chain.goto(11, 428, speed=int(245 * speed))
-chain.goto(12, 263, speed=int(511 * speed))
+def walk():
+    chain.goto(10, degreesToBits(150), speed=int(512 * speed))
+    chain.goto(11, degreesToBits(104), speed=int(245 * speed))
+    chain.goto(12, degreesToBits(32.2), speed=int(511 * speed))
 
-chain.goto(11, 467, speed=int(512 * speed))
-chain.goto(12, 291, speed=int(371 * speed))
+    chain.goto(10, degreesToBits(195), speed=int(512 * speed))
+    chain.goto(11, degreesToBits(125.6), speed=int(245 * speed))
+    chain.goto(12, degreesToBits(77.1), speed=int(511 * speed))
 
-chain.goto(10, 512, speed=int(512 * speed))
-chain.goto(11, 404, speed=int(209 * speed))
-chain.goto(12, 138, speed=int(509 * speed))
+    chain.goto(11, degreesToBits(137), speed=int(512 * speed))
+    chain.goto(12, degreesToBits(85.4), speed=int(371 * speed))
 
-chain.goto(10, 358, speed=int(512 * speed))
-chain.goto(11, 467, speed=int(209 * speed))
-chain.goto(12, 291, speed=int(509 * speed))
+    chain.goto(10, degreesToBits(150), speed=int(512 * speed))
+    chain.goto(11, degreesToBits(118.6), speed=int(209 * speed))
+    chain.goto(12, degreesToBits(40.6), speed=int(509 * speed))
 
-chain.goto(11, 428, speed=int(512 * speed))
-chain.goto(12, 263, speed=int(371 * speed))
+    chain.goto(10, degreesToBits(105), speed=int(512 * speed))
+    chain.goto(11, degreesToBits(137), speed=int(209 * speed))
+    chain.goto(12, degreesToBits(85.4), speed=int(509 * speed))
 
-chain.disable()
+    chain.goto(11, degreesToBits(125.6), speed=int(512 * speed))
+    chain.goto(12, degreesToBits(77.1), speed=int(371 * speed))
+
+    chain.disable()
+
+def main():
+    walk()
+
+if __name__ == '__main__':
+    main()
