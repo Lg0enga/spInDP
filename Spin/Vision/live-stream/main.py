c00e6d5638aeabc534aaf6b4aefb55eeb9678983
Mitchell Boorsma <mitchell.boorsma@gmail.com>
2016-06-06 13:34:18 +0000

    Basic streaming setup

---
+++ Spin/Vision/live-stream/main.py
@@ -0,0 +1,10 @@
+#foshizzlemanizzle
+from picamera import camera
+from stream import LiveStream
+
+stream = LiveStream((640,480), 28)
+
+if stream.start():
+	print "Connecting to server.."
+	while stream.streaming():
+		pass
